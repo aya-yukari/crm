59fd117fc27bb99e380e20cb6ad64e3fc4cccccf
aya-yukari <1394683742@qq.com>
2020-12-29 10:03:46 +0800

    crm03

---
+++ src/main/java/com/qf/crm/workbench/domain/ActivityRemark.java
@@ -0,0 +1,11 @@
+package com.qf.crm.workbench.domain;
+
+/**
+ * @ClassName ActivityRemark
+ * @Description: TODO
+ * @Author chenyu
+ * @Date 2020/12/29 10:03
+ * @Version 1.0
+ */
+public class ActivityRemark {
+}
